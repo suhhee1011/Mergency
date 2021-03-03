a39a9ff61c6a0d1eac6624285886adc62c570940
lmndang <lmndang.toronto@gmail.com>
2021-03-03 11:32:08 -0500

    Add database

--- app.js
+++ app.js
@@ -17,7 +17,46 @@ app.use(express.static("public"));
 app.engine("handlebars",exphbs());
 app.set("view engine", "handlebars");
 
+mongoose.connect("mongodb+srv://admin-lmndang:nhat2601@cluster0.e8pu8.mongodb.net/medicalDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
+const patientsSchema = {
+    firsrName: String,
+    lastName: String,
+    bof: String,
+    address: String,
+    phone: String,
+    insuranceId: String,
+    disability: String,
+    medication: String
+}
+
+const doctorsSchema = {
+    firsrName: String,
+    lastName: String,
+    address: String,
+    phone: String,
+    field: String
+}
+
+const helpersSchema = {
+    firsrName: String,
+    lastName: String,
+    address: String,
+    phone: String
+}
+
+const Helper = mongoose.model("helper", helpersSchema);
+
+const helper1 = Helper(
+    {
+        firsrName: "John",
+        lastName: "Smith",
+        address: "97 Clinton St, Toronto, ON M6G 2Y4",
+        phone: "4373335795"
+    }
+);
+
+//helper1.save();
 
 
 //app.use(fileUpload());
@@ -44,11 +83,11 @@ app.use("/",mainController);
 
 
 //MONGODB
-mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(()=>{
-    console.log(`Connected to MongoDB Database`);
-})
-.catch(err=>console.log(`Error occured when connecting to database ${err}`));
+// mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING, {useNewUrlParser: true, useUnifiedTopology: true})
+// .then(()=>{
+//     console.log(`Connected to MongoDB Database`);
+// })
+// .catch(err=>console.log(`Error occured when connecting to database ${err}`));
 
 //set up server
 const PORT = process.env.PORT||3000;
