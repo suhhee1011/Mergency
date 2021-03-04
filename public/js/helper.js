ec1203edfdfce7bd587ee5bd09c1cc723b1a01e6
unknown <suhhee1011@gmail.com>
2021-03-04 01:11:45 -0500

    hopital and helper icon added

--- public/js/helper.js
+++ public/js/helper.js
@@ -3,7 +3,7 @@ const map = L.map("map-template").setView([43.6532, -79.3832], 15);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-var myIcon = L.icon({
+var doctorIcon = L.icon({
     iconUrl: '../image/hospital.png',
     iconSize: [38, 50],
     iconAnchor: [22, 94],
@@ -11,8 +11,21 @@ var myIcon = L.icon({
     shadowSize: [68, 95],
     shadowAnchor: [22, 94]
 });
-const patientMarker = new L.marker([43.6532, -79.3832]).addTo(map).bindPopup('Helper: Nicholas');
-const helperMarker = new L.marker([43.6432, -79.3832]).addTo(map).bindPopup('Patient: Trang Nguyen');
-const doctorMarker = new L.marker([43.6402, -79.3832],{icon : myIcon}).addTo(map).bindPopup('Doctor: Suhhee Kim');
-
+var helperIcon = L.icon({
+    iconUrl: '../image/helper.png',
+    iconSize: [38, 50],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+    shadowSize: [68, 95],
+    shadowAnchor: [22, 94]
+});
+const patientMarker1 = new L.marker([43.6532, -79.3812]).addTo(map).bindPopup('Helper: Nicholas');
+const patientMarker2 = new L.marker([43.6542, -79.3830]).addTo(map).bindPopup('Helper: Bob');
+const patientMarker3 = new L.marker([43.6552, -79.3822]).addTo(map).bindPopup('Helper: James');
+const helperMarker1 = new L.marker([43.6532, -79.3802],{icon : helperIcon}).addTo(map).bindPopup('Patient: Trang Nguyen');
+const helperMarker2 = new L.marker([43.6502, -79.3852],{icon : helperIcon}).addTo(map).bindPopup('Patient: Ethan');
+const helperMarker3 = new L.marker([43.6512, -79.3822],{icon : helperIcon}).addTo(map).bindPopup('Patient: Abdula');
+const doctorMarker1 = new L.marker([43.6502, -79.3832],{icon : doctorIcon}).addTo(map).bindPopup('Doctor: Suhhee Kim');
+const doctorMarker2 = new L.marker([43.6502, -79.3802],{icon : doctorIcon}).addTo(map).bindPopup('Doctor: Suhhee Kim');
+const doctorMarker3 = new L.marker([43.6522, -79.3802],{icon : doctorIcon}).addTo(map).bindPopup('Doctor: Suhhee Kim');
     
